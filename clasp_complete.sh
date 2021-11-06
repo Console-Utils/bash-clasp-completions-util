aba3e02905f9a1790de8d8fc24fd6cbd1d5f896a
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-11-06 14:23:40 +1000

    .sh issue fix

--- clasp_complete.sh
+++ clasp_complete.sh
@@ -25,42 +25,42 @@ __clasp_complete()
 
   case ${COMP_CWORD} in
     1)
-      COMPREPLY=($(compgen -W "${SUBCOMMANDS[*]}" -- ${current}))
+      mapfile -t COMPREPLY < <(compgen -W "${SUBCOMMANDS[*]}" -- "${current}")
       ;;
     2)
       case ${previous} in
         login)
-          COMPREPLY=($(compgen -W "--no-localhost --creds" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "--no-localhost --creds" -- "${current}")
           ;;
         logout)
           COMPREPLY=()
           ;;
         create)
-          COMPREPLY=($(compgen -W "--type --title --rootDir --parentId" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "--type --title --rootDir --parentId" -- "${current}")
           ;;
         clone)
-          COMPREPLY=($(compgen -W "--versionNumber --rootDir" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "--versionNumber --rootDir" -- "${current}")
           ;;
         pull)
-          COMPREPLY=($(compgen -W "--versionNumber" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "--versionNumber" -- "${current}")
           ;;
         push)
-          COMPREPLY=($(compgen -W "-f --force -w --watch" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "-f --force -w --watch" -- "${current}")
           ;;
         status)
-          COMPREPLY=($(compgen -W "--json" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "--json" -- "${current}")
           ;;
         open)
-          COMPREPLY=($(compgen -W "--webapp --creds --addon --deploymentId" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "--webapp --creds --addon --deploymentId" -- "${current}")
           ;;
         deployments)
           COMPREPLY=()
           ;;
         deploy)
-          COMPREPLY=($(compgen -W "-V --versionNumber -d --description -i --deploymentId" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "-V --versionNumber -d --description -i --deploymentId" -- "${current}")
           ;;
         undeploy)
-          COMPREPLY=($(compgen -W "-a --all" -- ${current}))
+          mapfile -t COMPREPLY < <(compgen -W "-a --all" -- "${current}")
           ;;
         version)
           COMPREPLY=()
